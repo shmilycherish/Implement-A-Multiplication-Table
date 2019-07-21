d0f1d9058e5161c5ec518f7597b2b75dc57f427f
cyang <shmilycherish@gmail.com>
2019-07-21 22:09:38 +0800

    add function isValidMultiplicationPara

--- __tests__/multiplication-table-spec.js
+++ __tests__/multiplication-table-spec.js
@@ -1,4 +1,4 @@
-const {isValidStartEnd, isValidNumber} = require('../multiplication-table');
+const {isValidStartEnd, isValidNumber, isValidMultiplicationPara} = require('../multiplication-table');
 
 describe('test start should less than or equal end', () => {
     it('should return true when start less than end', () => {
@@ -35,6 +35,24 @@ describe('test input number is in valid range', () => {
 
     it('should return false when number is 1001, not between 1 and 1000', () => {
         expect(isValidNumber(1001)).toBeFalsy();
+    });
+});
+
+describe('test start and end are valid', () => {
+    it('should return true when start is 2 and end is 4', () => {
+        expect(isValidMultiplicationPara(2, 4)).toBeTruthy();
+    });
+
+    it('should return false when start is -1, out of range', () => {
+        expect(isValidMultiplicationPara(-1, 4)).toBeTruthy();
+    });
+
+    it('should return false when end is 1002, out of range', () => {
+        expect(isValidMultiplicationPara(2, 1002)).toBeTruthy();
+    });
+
+    it('should return false when start is 4 and end is 2, misorder', () => {
+        expect(isValidMultiplicationPara(4, 2)).toBeFalsy();
     });
 });
 
--- multiplication-table.js
+++ multiplication-table.js
@@ -6,7 +6,12 @@ function isValidNumber(input) {
     return input >= 1 && input <= 1000;
 }
 
+function isValidMultiplicationPara(start, end) {
+    return isValidNumber(start) && isValidNumber(end) && isValidStartEnd(start, end);
+}
+
 module.exports = {
     isValidStartEnd,
-    isValidNumber
-}
+    isValidNumber,
+    isValidMultiplicationPara
+};
